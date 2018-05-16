9f12dde5f0a128e45586674479d18431d3ce160e
Ваше Имя <you@example.com>
2018-05-16 10:05:52 +0700

    chess

---
+++ src/board.c
@@ -0,0 +1,13 @@
+#include "board_print_plain.h"
+#include <stdio.h>
+
+void board_print_plain(char deck[8][8]) {
+    for (int number = 0; number < 8; ++number) {
+        printf("%d ", 8 - number);
+        for (int letter = 0; letter < 8; ++letter) {
+            printf("%c ",deck[number][letter]);
+        }
+        printf("\n");
+    }
+    printf("  a b c d e f g h\n");
+}
